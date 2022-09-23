76a43d4d792f4c265f9177ff198668067b98f551
elbaley <lebafurkan@outlook.com>
2022-09-24 00:16:06 +0300

    add basic ui

--- app.js
+++ app.js
@@ -1,58 +1,50 @@
-const gameRounds = 5;
+const choiceButtons = document.querySelectorAll(".choice");
+const computerScoreSpan = document.querySelector("#computer");
+const playerScoreSpan = document.querySelector("#you");
+const roundResultText = document.querySelector(".round-text");
+const resultText = document.querySelector(".result-text");
+
 let playerScore = 0;
 let computerScore = 0;
 function getComputerChoice() {
   const options = ["rock", "paper", "scissors"];
   return options[Math.floor(Math.random() * options.length)];
 }
+function handleChoiceClick(e) {
+  playRound(e.target.innerText.toLowerCase(), getComputerChoice());
+}
+choiceButtons.forEach((choiceButton) => {
+  choiceButton.addEventListener("click", handleChoiceClick);
+});
 
 function playRound(playerSelection, computerSelection) {
   switch (true) {
     case playerSelection === computerSelection:
-      console.log(`Tie! ${playerSelection}-${computerSelection}`);
+      roundResultText.innerText = `Tie!`;
       break;
     // Player wins in the following conditions
     case playerSelection === "rock" && computerSelection === "scissors":
     case playerSelection === "scissors" && computerSelection === "paper":
     case playerSelection === "paper" && computerSelection === "rock":
       playerScore++;
-      console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+      playerScoreSpan.innerHTML = playerScore;
+      roundResultText.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
       break;
     default:
       computerScore++;
-      console.log(`You Lost! ${computerSelection} beats ${playerSelection}`);
+      computerScoreSpan.innerHTML = computerScore;
+      roundResultText.innerText = `You Lost! ${computerSelection} beats ${playerSelection}`;
       return;
   }
-}
 
-function game() {
-  //play rounds
-  for (let i = 0; i < gameRounds; i++) {
-    let playerSelection;
-    const computerSelection = getComputerChoice();
-    // ask for player selection
-    while (
-      playerSelection !== "rock" &&
-      playerSelection !== "paper" &&
-      playerSelection !== "scissors"
-    ) {
-      playerSelection = prompt(
-        "Select your weapon! (rock|paper|scissors)"
-      ).toLowerCase();
-    }
-    // Play the round
-    playRound(playerSelection, computerSelection);
-    // Log scores
-    console.log(`Player= ${playerScore} | Computer= ${computerScore}`);
-  }
-  //display the winner
-  if (computerScore === playerScore) {
-    console.log("GAME OVER! TIE!");
-  } else if (computerScore > playerScore) {
-    console.log("GAME OVER! Computer Wins!");
-  } else {
-    console.log("GAME OVER! Player Wins!");
+  //check if there is a winner
+  if (computerScore === 5 || playerScore === 5) {
+    //remove event listener
+    choiceButtons.forEach((choiceButton) => {
+      choiceButton.removeEventListener("click", handleChoiceClick);
+    });
+    //remove round result
+    roundResultText.innerText = "";
+    resultText.innerText = computerScore === 5 ? `Computer Wins!` : `You Win!`;
   }
 }
-//start the game
-game();
